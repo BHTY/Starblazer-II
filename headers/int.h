6d408269c9640c5f1ec381c298fcf35eb1765ac8
joshop <joshop4646@gmail.com>
2023-06-18 15:00:12 -0400

    linyos torovoltos 2

--- headers/int.h
+++ headers/int.h
@@ -7,8 +7,18 @@ typedef signed short int16;
 typedef unsigned short uint16;
 typedef signed int int32;
 typedef unsigned int uint32;
+typedef uint8 bool_t;
+
+#ifdef __GNUC__
+
 typedef signed long long int64;
 typedef unsigned long long uint64;
-typedef uint8 bool_t;
+
+#else
+
+typedef signed __int64 int64;
+typedef unsigned __int64 uint64;
+
+#endif
 
 #endif
