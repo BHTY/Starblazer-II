5a45dd7de995cb8645d5d4448014d0f3ae33ba0a
BHTY <phasershow@gmail.com>
2023-06-20 21:42:26 -0400

    dos port shenanigans

--- src/dosmake.bat
+++ src/dosmake.bat
@@ -1,4 +1,4 @@
-wcl386 -cc star_dos.c blazer.c graphics.c title.c sl_math.c font.c ui.c blazer2.c slipstr.c ecs.c hyptest.c -fe=star2.exe
+wcl386 -cc blazer.c graphics.c title.c sl_math.c font.c ui.c blazer2.c slipstr.c ecs.c hyptest.c star_dos.c -fe=star2.exe
 del *.obj
 del *.err
 copy star2.exe \release\star2.exe
--- src/star_dos.c
+++ src/star_dos.c
@@ -17,11 +17,70 @@ void setmode(char mode){ //sets the VGA card to the given display mode
 
 uint8* frontbuffer;
 
+unsigned char keys[256];
+static unsigned char kbdus[128] =
+{
+	0, 27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
+	'9', '0', '-', '=', '\b',     /* Backspace */
+	'\t',                 /* Tab */
+	'Q', 'W', 'E', 'r',   /* 19 */
+	't', 'y', 'u', 'i', 'o', 'P', '[', ']', '\n', /* Enter key */
+	0,                  /* 29   - Control */
+	'A', 'S', 'D', 'f', 'g', 'h', 'J', 'K', 'l', ';',     /* 39 */
+	'\'', '`', 0,                /* Left shift */
+	'\\', 'z', 'x', 'c', 'v', 'B', 'n',                    /* 49 */
+	'm', ',', '.', '/', 0,                              /* Right shift */
+	'*',
+	0,  /* Alt */
+	' ',  /* Space bar */
+	0,  /* Caps lock */
+	0,  /* 59 - F1 key ... > */
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0,  /* < ... F10 */
+	0,  /* 69 - Num lock*/
+	0,  /* Scroll Lock */
+	0,  /* Home key */
+	1,  /* Up Arrow */
+	0,  /* Page Up */
+	'-',
+	3,  /* Left Arrow */
+	0,
+	4,  /* Right Arrow */
+	'+',
+	0,  /* 79 - End key*/
+	2,  /* Down Arrow */
+	0,  /* Page Down */
+	0,  /* Insert Key */
+	0,  /* Delete Key */
+	0, 0, 0,
+	0,  /* F11 Key */
+	0,  /* F12 Key */
+	0,  /* All other keys are undefined */
+};
+
+
+void interrupt keyirq() {
+	char scancode = inp(0x60);
+
+	if (scancode & 0x80){ //key has been released
+		keys[kbdus[scancode - 0x80]] = 0;
+	}
+
+	else{ //key has been pressed
+		keys[kbdus[scancode]] = 1;
+	}
+	outp(0x20, 0x20);
+}
+
 void SG_Init(int argc, char** argv){
+	memset(keys, 0, 256);
+
 	SG_GameInit();
 	frontbuffer = (char*)0xA0000L;
 	FBPTR = malloc(64000);
 	setmode(0x13);
+
+	_dos_setvect(9, keyirq);
 }
 
 void SG_Close(){
@@ -36,10 +95,11 @@ void SG_ReadMouse(SG_mouse_t* mouse){
 	memset(mouse, 0, sizeof(SG_mouse_t));
 	mouse->x = 160;
 	mouse->y = 100;
+	mouse->buttons[0] = SG_KeyDown(' ');
 }
 
 bool_t SG_KeyDown(char key){
-	return 0;
+	return keys[key];
 }
 
 void SG_DrawFrame(){
