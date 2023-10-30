77d37f1fdbc37acfba43a2ac41878ebcd0b7075f
BHTY <phasershow@gmail.com>
2023-10-29 20:23:47 -0400

    dos improvements

--- src/dosmake.bat
+++ src/dosmake.bat
@@ -1,4 +1,7 @@
-wcl386 -cc blazer.c graphics.c title.c sl_math.c font.c ui.c blazer2.c slipstr.c ecs.c hyptest.c sndmixer.c net_dos.c star_dos.c -ot -fe=star2.exe > output.txt
+wcl386 -c -cc -ot -bcl=dos32a blazer.c graphics.c title.c sl_math.c font.c ui.c blazer2.c
+wcl386 -c -cc -ot -bcl=dos32a slipstr.c ecs.c hyptest.c sndmixer.c net_dos.c star_dos.c
+wcl386 -cc -ot -bcl=dos32a *.obj
+
 del *.obj
 del *.err
 copy blazer.exe \release\blazer.exe
